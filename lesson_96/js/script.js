69c4995da01af298240f6e828918f096ebb07ba4
Vlad <129100867+sokovlad@users.noreply.github.com>
2024-07-25 21:18:48 +0300

    update js file

--- lesson_96/js/script.js
+++ lesson_96/js/script.js
@@ -2,6 +2,8 @@
   const burger = document.querySelector(".burger-menu");
   const links = document.querySelector(".header__list");
   const body = document.querySelector(".body");
+  const gift = document.querySelector(".about__btn-play");
+  const modal = document.querySelector(".modal");
   burger.addEventListener("click", (e) => {
     body.classList.toggle("body--opened-menu");
   });
@@ -12,6 +14,17 @@
       body.classList.contains("body--opened-menu")
     ) {
       body.classList.remove("body--opened-menu");
+    }
+  });
+  gift.addEventListener("click", (e) => {
+    e.preventDefault();
+    body.classList.add("body--opened-modal");
+  });
+  modal.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = e.target;
+    if (target == modal || target.closest(".modal__cancel")) {
+      body.classList.remove("body--opened-modal");
     }
   });
 })();
