52457b775919b026ec59c819ab299598d6cb5ec2
Vlad <129100867+sokovlad@users.noreply.github.com>
2024-07-28 11:37:20 +0300

    update project

--- lesson_96/js/script.js
+++ lesson_96/js/script.js
@@ -54,6 +54,7 @@
   const accordionLists = document.querySelectorAll(".accordion-list");
   accordionLists.forEach((el) => {
     el.addEventListener("click", (e) => {
+      e.preventDefault();
       const accordionList = e.currentTarget;
       const accordionOpenedItem = accordionList.querySelector(
         ".accordion-list__item--opened"
@@ -139,3 +140,7 @@ const swiper2 = new Swiper(".feedback__slider", {
     },
   },
 });
+// Маска для инпута
+const telInputs = document.querySelectorAll('input[type="tel"]');
+const im = new Inputmask("+7 (999) 999-99-99");
+im.mask(telInputs);
